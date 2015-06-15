d9f47a79c4998d7ef516beb7e4b41e437ffc893f
chadec <chad@dubbysattractions.com>
2015-06-15 10:19:55 -0400

    Initial Commit

---
+++ Destiny Status Monitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Destiny Status Monitor
+//
+//  Created by Chad Cardella on 6/15/15.
+//  Copyright © 2015 Chad Cardella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
